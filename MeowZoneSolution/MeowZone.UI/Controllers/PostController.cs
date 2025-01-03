001f44c69796acedd7a5a6c34993bdab2473de55
Kropa <k0n42525@gmail.com>
2025-01-03 22:19:57 +0100

    Added PostController with Views

---
+++ MeowZoneSolution/MeowZone.UI/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowZone.UI.Controllers
+{
+	public class PostController : Controller
+	{
+		public IActionResult CreatePost()
+		{
+			return View();
+		}
+		public IActionResult EditPost()
+		{
+			return View();
+		}
+
+		public IActionResult DeletePost()
+		{
+			return View();
+		}
+
+		public IActionResult ShowPost()
+		{
+			return View();
+		}
+
+		public IActionResult ListPosts()
+		{
+			return View();
+		}
+	}
+}
