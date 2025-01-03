174c45669f687fd2b07277f8de7d30ecb441f8f7
Kropa <k0n42525@gmail.com>
2025-01-03 19:30:27 +0100

    added Post class and added attributes to Cat class

--- MeowZoneSolution/MeowZone.Core/Domain/Entities/Cat.cs
+++ MeowZoneSolution/MeowZone.Core/Domain/Entities/Cat.cs
@@ -1,4 +1,5 @@
 ﻿using MeowZone.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MeowZone.Models
 {
@@ -7,12 +8,25 @@ namespace MeowZone.Models
         /// <summary>
         /// Domain Model for Cat
         /// </summary>
+        [Key]
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Range(0.1, 50.0)]
         public decimal Weight { get; set; }
+
+        [Range(0, 30)]
         public int Age { get; set; }
+
+        [StringLength(50)]
         public string? Breed { get; set; }
+
+        [StringLength(30)]
         public string? Color { get; set; }
+
         public GenderOptions? Gender { get; set; }
     }
 }
---
+++ MeowZoneSolution/MeowZone.Core/Domain/Entities/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeowZone.Core.Domain.IdentityEntities;
+
+namespace MeowZone.Core.Domain.Entities
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("Author")]
+        public Guid AuthorId { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        [StringLength(500)]
+        public string Summary { get; set; }   
+
+        public bool IsPublished { get; set; }
+    }
+}
