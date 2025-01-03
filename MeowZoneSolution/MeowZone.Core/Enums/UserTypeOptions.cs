4bfb9b31637d5e11537261540d9b1ea0156143d1
Kropa <k0n42525@gmail.com>
2025-01-03 19:33:34 +0100

    Added UserTypeOptions enum

---
+++ MeowZoneSolution/MeowZone.Core/Enums/UserTypeOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowZone.Core.Enums
+{
+     public enum UserTypeOptions
+    {
+        Admin, Moderator, User
+    }
+}
