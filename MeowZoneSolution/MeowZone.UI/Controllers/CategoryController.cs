b7e5d51d8215e5f53c9785d79291ce1cbb25b5a8
Kropa <k0n42525@gmail.com>
2025-01-03 21:42:17 +0100

    Fixes for stylesheet and added CategoryController

---
+++ MeowZoneSolution/MeowZone.UI/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowZone.UI.Controllers
+{
+	[Route("[controller]/[action]")]
+	public class CategoryController : Controller
+	{
+		public IActionResult ShowCategories()
+		{
+			return View();
+		}
+	}
+}
