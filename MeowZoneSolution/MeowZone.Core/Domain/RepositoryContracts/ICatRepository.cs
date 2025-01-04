e360d5886f922811fefee947c9dc635594dd899b
Kropa <k0n42525@gmail.com>
2025-01-04 09:08:09 +0100

    Added Category class and ICategoryRepository interface

--- MeowZoneSolution/MeowZone.Core/Domain/RepositoryContracts/ICatRepository.cs
+++ MeowZoneSolution/MeowZone.Core/Domain/RepositoryContracts/ICatRepository.cs
@@ -23,18 +23,26 @@ namespace MeowZone.Core.Domain.RepositoryContracts
         /// <returns>Returns the updated cat object</returns>
         Task<Cat> UpdateCat(Cat cat);
 
-        /// <summary>
-        /// Deletes a cat based on the cat id 
-        /// </summary>
-        /// <param name="cat">CatID (guid) to delete</param>
-        /// <returns></returns>
-        Task<Cat> DeleteCat(Guid catID);
+		/// <summary>
+		/// Deletes a cat based on the cat id 
+		/// </summary>
+		/// <param name="cat">CatID (guid) to delete</param>
+		/// <returns>Return true if the deletion is successful; otherwise, return false.</returns>
+		Task<Cat> DeleteCat(Guid catID);
 
         /// <summary>
         /// Returns a cat object based on the given cat id 
         /// </summary>
         /// <param name="catID">CatID (guid) to search</param>
-        /// <returns></returns>
+        /// <returns>A cat object or null</returns>
         Task<Cat> GetCatByCatID(Guid catID);
+
+		/// <summary>
+		/// Returns a list of cats based on the given user ID
+		/// </summary>
+		/// <param name="UserID">UserID to search by</param>
+		/// <returns>A cat object or null</returns>
+
+		Task<List<Cat>> GetCatsByUserID(Guid UserID);
     }
 }
