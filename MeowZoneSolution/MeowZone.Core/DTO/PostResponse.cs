fb668a2eff4cf4a6a0fc4ee89eaf5aa48a3eb9ec
Kropa <k0n42525@gmail.com>
2025-01-05 12:25:03 +0100

    Added identity DTOs

---
+++ MeowZoneSolution/MeowZone.Core/DTO/PostResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowZone.Core.DTO
+{
+	public class PostResponse
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string Summary { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public Guid AuthorId { get; set; }
+		public string AuthorName { get; set; }
+		public Guid CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public int ViewCount { get; set; }
+		public bool IsPublished { get; set; }
+	}
+}
---
+++ MeowZoneSolution/MeowZone.Core/DTO/PostUpdateRequest.cs
@@ -0,0 +1,39 @@
+﻿using MeowZone.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeowZone.Core.DTO
+{
+	public class PostUpdateRequest
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(200, ErrorMessage = "Title cannot exceed 200 characters.")]
+		public string Title { get; set; }
+
+		[Required]
+		[MinLength(20, ErrorMessage = "Content must be at least 20 characters long.")]
+		public string Content { get; set; }
+
+		[StringLength(500, ErrorMessage = "Summary cannot exceed 500 characters.")]
+		public string Summary { get; set; }
+
+		public bool IsPublished { get; set; }
+		public Post ToPost()
+		{
+			return new Post()
+			{
+				Title = Title,
+				Content = Content,
+				Summary = Summary,
+				IsPublished = IsPublished
+			};
+		}
+	}
+}
