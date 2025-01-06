1b1ee62d43756a1cc2f8b35f7dcd34bf4a2f2e79
Kropa <k0n42525@gmail.com>
2025-01-06 03:32:39 +0100

    Added CatDeleterService

---
+++ MeowZoneSolution/MeowZone.Core/Services/CatDeleterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeowZone.Core.Domain.RepositoryContracts;
+using MeowZone.Core.ServiceContracts;
+using MeowZone.Models;
+
+namespace MeowZone.Core.Services
+{
+	public class CatDeleterService : ICatDeleterService
+	{
+		private readonly ICatRepository _catRepository;
+
+		public CatDeleterService(ICatRepository catRepository)
+		{
+			_catRepository = catRepository;
+		}
+		public async Task<bool> DeleteCat(Guid id)
+		{
+			if (id == null)
+			{
+				throw new ArgumentNullException(nameof(id));
+			}
+
+			Cat? cat = await _catRepository.GetCatByCatId(id);
+			if (cat == null)
+			{
+				return false;
+			}
+
+			await _catRepository.DeleteCat(id);
+
+			return true;
+		}
+	}
+}
